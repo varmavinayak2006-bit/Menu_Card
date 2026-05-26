429665f8bcc53417b1aeb0a1e651d4d459e769fe
Vinayak2006 <varmavinayak2006@gmail.com>
2026-05-26 16:31:04 +0530

    Output for Viewr

---
+++ menu.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main ()
+{
+    double Total_bill = 0 ;
+    char Nxt_Order ;
+    string name ;
+    cout<<"Please Enter Your Name : "<<name<<endl;
+    cin>>name;
+    cout<<"Hello "<<name<<" Welcome to our E- Cafe ! "<<endl;
+    cout<<"Here is our E-Menu "<<endl;
+    cout<<" 1.SNACKS :"<<endl;
+    cout<<"     a. SAMOSA --  20 ₹"<<endl;
+    cout<<"     b. FRIES ---- 80 ₹"<<endl;
+    cout<<"     c. SANDWICH - 60 ₹"<<endl<<endl<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<" 2.SOFT DRINKS :"<<endl;
+    cout<<"     a. COLA ----- 40 ₹"<<endl;
+    cout<<"     b. LEMONADE - 35 ₹"<<endl<<endl<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<" 3.MOCKTAILS :"<<endl;
+    cout<<"     a. VIRGIN MOJITO ----- 120 ₹"<<endl;
+    cout<<"     b. BLUE LAGOON ------- 140 ₹"<<endl<<endl<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<" 4.JUICES :"<<endl;
+    cout<<"     a. ORANGE --- 60 ₹"<<endl;
+    cout<<"     b. MANGO ---- 70 ₹"<<endl<<endl<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<" 5.TEA :"<<endl;
+    cout<<"     a. MASALA --- 15 ₹"<<endl;
+    cout<<"     b. GREEN ---- 25 ₹"<<endl;
+    cout<<"     c. GINGER --- 20 ₹"<<endl<<endl<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<" 6.COFFEE :"<<endl;
+    cout<<"     a. ESPRESSO ----------- 50 ₹"<<endl;
+    cout<<"     b. CAPPUCCINO --------- 80 ₹"<<endl;
+    cout<<"     c. LATTE -------------- 85 ₹"<<endl<<endl<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<" 7.STARTERS :"<<endl;
+    cout<<"     a. PANEER TIKKA ------ 150 ₹"<<endl;
+    cout<<"     b. VEG CRISPY -------- 120 ₹"<<endl<<endl<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<"Plesae place your order as CHOICE for 1-7 and SUB CHOICE for a-c as per the AVAILABILITY";
+
+    
+
+}
