2b1aea8fb6f58ae30c55bfed623b824a0bbf8734
Vinayak2006 <varmavinayak2006@gmail.com>
2026-05-27 18:36:18 +0530

    Finallisation with output receipt

--- menu.cpp
+++ menu.cpp
@@ -5,9 +5,10 @@ int main ()
     double Total_bill = 0 ;
     char Nxt_Order ;
     string name ;
-    cout<<"Please Enter Your Name : "<<name<<endl;
-    cin>>name;
-    cout<<"Hello "<<name<<" Welcome to our E- Cafe ! "<<endl;
+    cout<<"Please Enter Your Name : "<<endl;
+    cin >> ws ;
+    getline(cin, name);
+    cout<<endl<<"Hello "<<name<<" Welcome to our E- Cafe ! "<<endl;
     cout<<"Here is our E-Menu "<<endl;
     cout<<" 1.SNACKS :"<<endl;
     cout<<"     a. SAMOSA --  20 ₹"<<endl;
@@ -43,7 +44,160 @@ int main ()
     cout<<"=================================================================================="<<endl;
     cout<<"=================================================================================="<<endl;
     cout<<"Plesae place your order as CHOICE for 1-7 and SUB CHOICE for a-c as per the AVAILABILITY";
+    do {    
+        char sub_choice ;
+        int choice ;
+        int quantity ;
+        cin>>choice;
+        cin>>sub_choice;
+        cout<<"Enter the quantity ."<<endl;
+        cin>>quantity;
+             if (quantity < 0)
+              {
+        cout << "Invalid quantity! Defaulting to 1." << endl;
+        quantity = 1;
+              }
+            switch (choice)
+            {
+            case 1:
+                cout<<"SNACKS\n";
+                switch (sub_choice)
+                {
+                    case 'a' :
+                    Total_bill = Total_bill + 20 * quantity ;
+                    break ;
+                    case 'b' :
+                    Total_bill = Total_bill + 80 * quantity ;
+                    break ;
+                    case 'c' :
+                    Total_bill = Total_bill + 60 * quantity ;
+                    break ;
+                    default :
+                    cout<<"INCORRECT INPUT\n";
+                    break ;
+                }
+                break;
+            case 2 :
+                cout<<"SOFT DRINKS\n";
+                switch (sub_choice)
+                {
+                    case 'a' :
+                    Total_bill = Total_bill + 40 * quantity ;
+                    break;
+                    case 'b' :
+                    Total_bill = Total_bill + 35 * quantity ;
+                    break ;
+                    default :
+                    cout<<"INCORRECT INPUT\n";
+                    break ;
+                }
+                break;
+            case 3 :
+                cout<<"MOCKTAILS\n";
+                switch (sub_choice) 
+                {
+                    case 'a' :
+                    Total_bill = Total_bill + 120 * quantity ;
+                    break ;
+                    case 'b' :
+                    Total_bill = Total_bill + 140 * quantity ;
+                    break ;
+                    default :
+                    cout<<"INCORRECT INPUT\n";
+                    break ;
+                }
+                break;
+            case 4 :
+                cout<<"JUICES\n";
+                 switch (sub_choice) 
+                 {
+                    case 'a' :
+                    Total_bill = Total_bill + 60 * quantity ;
+                    break ;
+                    case 'b' :
+                    Total_bill = Total_bill + 70 * quantity ;
+                    break ;
+                    default :
+                    cout<<"INCORRECT INPUT\n";
+                    break ;   
+                 }
+                 break;
+            case 5 :
+                 cout<<"TEA\n";
+                 switch (sub_choice)
+                 {
+                    case 'a' :
+                    Total_bill = Total_bill + 15 * quantity ;
+                    break ;
+                    case 'b' :
+                    Total_bill = Total_bill + 25 * quantity ;
+                    break ;
+                    case 'c' :
+                    Total_bill = Total_bill + 20 * quantity ;
+                    break ;
+                    default :
+                    cout<<"INCORRECT INPUT\n";
+                    break ;
+                 }
+                 break;
+            case 6 :
+                 cout<<"COFFEE\n";
+                 switch(sub_choice)
+                 {
+                    case 'a' :
+                    Total_bill = Total_bill + 50 * quantity ;
+                    break ;
+                    case 'b' :
+                    Total_bill = Total_bill + 80 * quantity ;
+                    break ;
+                    case 'c' :
+                    Total_bill = Total_bill + 85 * quantity ;
+                    break ;
+                    default :
+                    cout<<"INCORRECT INPUT\n";
+                    break ;
+                 }
+                 break ;
+            case 7 :
+                 cout<<"STARTERS\n";
+                 switch (sub_choice)
+                 {
+                    case 'a' :
+                    Total_bill = Total_bill + 150 * quantity ;
+                    break ;
+                    case 'b' :
+                    Total_bill = Total_bill + 120 * quantity ;
+                    break ;
+                    default :
+                    cout<<"INCORRECT INPUT\n";
+                    break ;
+                 }
+                 break ;
+            }
+            cout<<"FOR NEXT OREDER PLEASE ENTRY 'Y' OR ELSE 'N' : "<<endl;
+            cin>>Nxt_Order ;
 
-    
+    } while ( Nxt_Order == 'Y' || Nxt_Order == 'y' ) ;
+    double SGST_Am = Total_bill * 0.025 ; 
+    double CGST_Am = Total_bill * 0.025 ; 
+    double Platform_Charges = 0.01 * Total_bill ;
 
+    double Final_Payable = Total_bill + SGST_Am + CGST_Am + Platform_Charges ;
+
+    cout<<endl<<endl<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<"--------------------------- THANKS FOR VISITING ----------------------------------"<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<"=================================================================================="<<endl;
+    cout<<"                         E - CAFE DIGITAL RECEIPT                                 "<<endl;
+    cout<<" CUSTOMER NAME : "<<name<<endl;
+    cout<<" SUB TOTAL OF BILL : "<<Total_bill<<endl;
+    cout<<" CGST and SGST TAX ( 2.5 %) : " <<endl;
+    cout<<" CGST : "<<CGST_Am<<endl<<" SGST : "<<SGST_Am<<endl;
+    cout<<" STAFF FEES and PLATFORM CHARGES: "<<Platform_Charges<<endl<<endl<<endl;
+    cout<<"                        YOUR FINAL BILL IS  : "<<Final_Payable<<endl<<endl<<endl;
+    cout<<"              It Was A Great PLeasure For Us To Dine With You.  "<<endl;
+    cout<<"                       Have A Wonderful Day Ahead ! "<<endl;
+
+    return 0 ;
 }
